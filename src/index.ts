517c0d878a2da4bcb55eb8fc1eca8824e82f98c5
Camilo Galindo R <cagalindo35@misena.edu.co>
2022-10-02 11:44:36 -0500

    fix host

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import "reflect-metadata";
+require('dotenv').config();
 import app from './server';
 import { AppDataSource } from "./database/connection";
+
+const PORT : number = process.env.PORT ? +process.env.PORT : 4000;
 
 async function main() {
 	try {
 		await AppDataSource.initialize();
-		app.listen(4000, () => console.log('Server listening on port 4000'));
+		app.listen(PORT, '0.0.0.0', () => console.log('Server listening on port', PORT));
 	} catch(e) {
 		console.error(e);
 	}
